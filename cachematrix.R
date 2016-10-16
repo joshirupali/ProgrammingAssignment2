d5ac4720d969ff9153b51bfd5dcfaab200d46894
Rupali Joshi <joshi.rupali06@gmail.com>
2016-10-16 17:17:39 +0530

    assignment week 3 done

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,45 @@
 
-?solve
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##'makeCacheMatrix' function takes a matrix as an arguement which sets and gets the matrix as well as sets and gets the inverse of matrix.
+##it returns a list of 4 function which in turn return the matrix when called.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
-  setmat <- function(y=matrix()) {
-    x <<- y
+  setmat <- function(y=matrix()) {   ##sets the value of the matrix
+    x <<- y        
     m <<- NULL
   }
-  getmat <- function(){ x }
+  getmat <- function(){ x }       ##gets the value of the matrix
   
-  setinv<-function(inv){m<<-inv}
-  getinv<-function() m
+  setinv<-function(inv){m<<-inv}      ##sets the value of the matinverse of matrix
+  getinv<-function() m                ##gets the value of the matinverse of matrix
     list(setmat = setmat,
          getmat = getmat,
          setinv = setinv,
-         getinv = getinv)
+         getinv = getinv)          ##returns list
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' function checks if the inverse is already calculated, if not returns the inverse of the matrix.
 
 cacheSolve <- function(x, ...) {
-    callmat<-x$getinv
-  data1 <- x$getmat()
-  callmat <- solve(data1, ...)
-  x$setinv(callmat)
-  callmat
+    callmat<-x$getinv  ##call to getinv() if inverse is present
   
-          ## Return a matrix that is the inverse of 'x'
+      if(!is.null(callmat)) {       ##checks if inverse is not null and returns the available inverse. 
+      message("getting cached data")
+      return(callmat)
+    }
+    
+  data1 <- x$getmat()            ##gets the matrix which was passed in 'makeCacheMatrix'
+  callmat <- solve(data1, ...)        ##calcutes the inverse using solve function
+  x$setinv(callmat)     ##sets the new inverse of matrix
+  callmat  ## Return a matrix that is the inverse of 'x'
+  
+          
 }
 
-
+## function call
 matt<-makeCacheMatrix(matrix(c(2,4,5,3),2,2))
-# matt$getmat()
-# matt$setmat(matrix(1:4,2,2))
 
 cacheSolve(matt)
 
